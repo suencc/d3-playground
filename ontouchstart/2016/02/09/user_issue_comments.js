0048311f9e3a54db6886822ab815c865db075ed7
ontouchstart <ontouchstart@gmail.com>
2016-02-10 16:07:43 -0500

    user_issue_comments.js

---
+++ ontouchstart/2016/02/09/user_issue_comments.js
@@ -0,0 +1,31 @@
+user_issue_comments = function (user, issue) {
+  var api = 'https://api.github.com/repos/bigdata-mindstorms/d3-playground/issues/' + issue + '/comments?per_page=100';
+  d3.json(api, function (e, d) { 
+    if(!e) { 
+      d3.select('body')
+        .append('p')
+        .append('a')
+        .attr({ href : 'https://github.com/' + user })
+        .html('Comments by ' + user);
+      d3.select('body')
+        .append('p')
+        .append('a')
+        .attr({ href : 'https://github.com/bigdata-mindstorms/d3-playground/issues/' + issue})
+        .html('on issue ' + issue);
+      d.forEach(function (d) {
+        if(d.user.login === user) {
+          console.log(d.body);
+          d3.select('body')
+            .append('pre')
+            .html(d.body)
+            .style({background: '#ccc', padding: '1em'});
+          d3.select('body')
+            .append('div')
+            .html(marked(d.body));
+        }
+      });
+    }
+  });
+}
+
+// user_issue_comments('ontouchstart', 21);
