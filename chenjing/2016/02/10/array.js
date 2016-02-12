2ace27f5e0de90d84606f15ca850a033c3d41cfd
JingCHEN <dianachenfr@gmail.com>
2016-02-12 15:28:22 +0800

    array

---
+++ chenjing/2016/02/10/array.js
@@ -0,0 +1,12 @@
+var years = [1950, 1960, 1970, 1980, 1990, 2000, 2010];
+console.log(years[0]);
+years.shift();
+console.log(years[0]);
+years.push(2020);
+console.log(years[years.length-1]);
+
+var animal={
+  'cat':'Katy',
+  'tiger':'HelloKity'
+};
+console.log(animal['tiger']);
