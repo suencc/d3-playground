32828c286d640e06bdcaf6fba95a6b75283b413f
farname <rengz@uxsino.com>
2016-02-13 21:59:34 +0800

    Create selector.js

---
+++ farname/selector.js
@@ -0,0 +1,7 @@
+var box = d3.select("body")
+            .append("svg")
+            .attr("width", 300)
+            .attr("height", 50).style("background","#60f");
+box.append("text")
+            .text("O(∩_∩)O")
+            .attr({"x":130,"y":25,fill:"white"})
