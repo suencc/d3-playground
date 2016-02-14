c90b7c0fe6fd2edcd4f3703b556ce7147e234bb9
double126 <172127079@qq.com>
2016-02-14 21:51:18 +0800

    Update fun.js

--- double126/2016/02/14/fun.js
+++ double126/2016/02/14/fun.js
@@ -1,9 +1,15 @@
 function add_radio(selection, name, checked , value) {
-  selection.append('input').attr('type', "radio").attr('name',name).attr('value',value).attr('checked',checked)
+  var radio_sel = selection.append('input').attr('type', "radio").attr('name',name).attr('value',value);
+
+  console.log(checked);
+
+  if(checked == "true"){
+    console.log(value +' checked true');
+    radio_sel.attr('checked',true);
+  }
 
 }
 
-
 d3.select('body').append('p').html('性别：');
-add_radio(d3.select('body').append('p').html('男：'), "sex", "checked", "male");
-add_radio(d3.select('body').append('p').html('女：'), "sex", "", "female");
+add_radio(d3.select('body').append('p').html('男：'), "sex", "true", "male");
+add_radio(d3.select('body').append('p').html('女：'), "sex", "false", "female");
