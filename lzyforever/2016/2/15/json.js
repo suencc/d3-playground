f5fd1880a523fa135babc14cebc9dad368a18973
lzyforever <261087685@qq.com>
2016-02-15 16:13:27 +0800

    Update json.js

--- lzyforever/2016/2/15/json.js
+++ lzyforever/2016/2/15/json.js
@@ -3,5 +3,7 @@ jsonArry.push("保时捷");
 jsonArry.push('兰博基尼');
 jsonArry.push("英飞尼迪");
 console.log(jsonArry);
-var jstObj = JSON.stringify(jsonArry);
-console.log(jstObj);
+
+var jsObj = JSON.stringify(jsonArry);
+
+d3.select("body").append("p").html(jsObj);
