f5036f5da4e129b6e753db6f3868f6e5dc23ccaa
lzyforever <261087685@qq.com>
2016-02-15 14:05:45 +0800

    Create array.js

---
+++ lzyforever/2016/2/15/array.js
@@ -0,0 +1,17 @@
+d3.select("body").append("h1").html("汽车品牌");
+
+function print(array){
+  selection = d3.select("body").append("table");
+  array.forEach(function(item,index){
+    tr = selection.append("tr");
+    td_user(tr, item); 
+  })
+}
+
+function td_car(selection,array){
+  tr.append("td").html(array.id);
+  tr.append("td").html(array.userName);
+}
+
+cars = [{"id":1,"carName":"保时捷"},{"id":2,"carName":"兰博基尼"},{"id":3,"carName":"英飞尼迪"}];
+print(cars);
