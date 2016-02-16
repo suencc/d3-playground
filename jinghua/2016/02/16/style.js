d2488eea41e08f14556bb7ce85f9a01233784e62
jinghuaa <liujh@uxsino.com>
2016-02-16 17:16:00 +0800

    Create style.js

---
+++ jinghua/2016/02/16/style.js
@@ -0,0 +1,8 @@
+d3.select("body").append("div").append("p").html("Hello Word")
+  .style({"color":"red","font-size":"x-small"});
+d3.select("body").append("div").append("p").html("Hello Word")
+  .style({"color":"yellow","font-size":"small"});
+d3.select("body").append("div").append("p").html("Hello Word")
+  .style({"color":"green","font-size":"large"});
+d3.select("body").append("div").append("p").html("Hello Word")
+  .style({"color":"blue","font-size":"x-large"});
