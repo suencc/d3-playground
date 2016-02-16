ed54ab0fa5df07379f37231f8f7633641d32743d
xiaoer <xiaoer_2013@sina.cn>
2016-02-16 16:53:51 +0800

    Create json.js

---
+++ chenxiaorong/2016/02/16/json.js
@@ -0,0 +1,8 @@
+array_num = [];
+array_num.push("你好");
+array_num.push(2016);
+array_num.push(true);
+array_num.push({"name":"优炫软件"});
+console.log(array_num);
+var name = JSON.stringify(array_num);
+console.log(name);
