38b0338179d330714c79adc61c184c557bd4681e
jinghuaa <liujh@uxsino.com>
2016-02-16 15:59:26 +0800

    Create element_a.js

---
+++ jinghua/2016/02/16/element_a.js
@@ -0,0 +1,7 @@
+d3.select('body')
+	.append('a')
+	.attr('href','http://www.baidu.com')
+	.html('百度<br/>').style('color','grey')
+	.append('a')
+	.attr('href','http://www.sina.com.cn')
+	.html('新浪<br/>').style('color','blur');
