c77fb8b1e96a9b77ac4bd2f3ba3a17f66e11922b
keeping-action <461006791@qq.com>
2016-02-17 00:02:19 +0800

    Create json.js

---
+++ keeping-action/json/json.js
@@ -0,0 +1,9 @@
+var language=[];
+var JAVA=new Array();
+JAVA.push('JDK1.5');
+JAVA.push('JDK1.6');
+language.push(JAVA);
+language.push('C');
+language.push('PHP');
+console.log(language);
+console.log(JSON.stringify(language));
