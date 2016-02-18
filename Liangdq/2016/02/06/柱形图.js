10900e32e0f463218d842c49376b0a9f2c79a121
piKapiKa <419910312@qq.com>
2016-02-18 23:18:27 +0800

    Update 柱形图.js

--- Liangdq/2016/02/06/柱形图.js
+++ Liangdq/2016/02/06/柱形图.js
@@ -1,22 +1,16 @@
 var 销售额 = [200,178,213,330,150,300,295,177,260,340,250,322];
 var 月份 = [1,2,3,4,5,6,7,8,9,10,11,12];
 
-var xordinal = d3.scale.ordinal().domain(月份).rangeRoundBands([0, 480]);
-var ylinear = d3.scale.linear().domain([d3.min(销售额),d3.max(销售额)]);
-console.log("****");
-console.log(ylinear,range(););
-console.log(xordinal.range(););
-console.log("****");
+var blank = {top:20,right:20,bottom:20,left:50};
+var svg = d3.select("body").append("svg").attr("width", 600) .attr("height", 600).append("g").attr("transform",'translate(' + blank.left + ',' + blank.top + ')');
 
-var svg = d3.select("body").append("svg").attr("width", 500) .attr("height", 500);
+var xordinal = d3.scale.ordinal().domain(月份).rangeRoundBands([0, 500]);
+var ylinear = d3.scale.linear().domain([0,d3.max(销售额)]).range([500,0]);
 
 
 var xaxis =  d3.svg.axis().scale(xordinal).orient("bottom");
 var yaxis =  d3.svg.axis().scale(ylinear).orient("left");
-svg.append("g").call(xaxis);
+svg.append("g").call(xaxis).attr('transform', 'translate(0,' + 500+ ')');
 svg.append("g").call(yaxis);
 
-
-
-var rects = svg.selectAll("rect").data(销售额).enter().append("rect")
-.attr("x", function(d,i){ return xordinal(i) + 10; } ).attr("y",function(d,i){ return ylinear(d); }).attr("width", xordinal.rangeBand() - 20 ).attr("height", function(d,i){ return ylinear(d); });
+var rects = svg.selectAll("rect").data(销售额).enter().append("rect").attr("x", function(d,i){ return xordinal(i)+40; } ).attr("y",function(d,i){ return ylinear(d); }).attr("width", xordinal.rangeBand() - 10 ).attr("height", function(d,i){ return 500 - ylinear(d) ; });
