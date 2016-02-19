4f91057becb0197424e866cd1b6296a2c350b83e
ontouchstart <ontouchstart@gmail.com>
2016-02-19 17:54:31 -0500

    https://bigdata-mindstorms.github.io/d3-playground/ontouchstart/2016/02/20/ColorKeywords

--- ontouchstart/2016/02/20/ColorKeywords/index.js
+++ ontouchstart/2016/02/20/ColorKeywords/index.js
@@ -4,31 +4,27 @@ d3.select('head').append('style').html('div { padding: 1em; }');
 d3.select('head').append('style').html('div { text-align: center; }');
 
 function list(container, data) { 
+  function color(selection, color, background) {
+    selection.style({ 
+      color: color,
+      background: background
+    });
+  }
+
+  function compare_color(container, d) {
+    color(container.append('div').html(d), d, 'white');
+    color(container.append('div').html(d), d, 'black');
+    color(container.append('div').html(d), 'white', d);
+    color(container.append('div').html(d), 'black', d);
+  }
+
   container.selectAll('div')
     .data(d3.keys(data))
     .enter()
     .append('div')
     .each(function (d) {
-       d3.select(this)
-         .append('div')
-         .html(function(d) { return d; })
-         .style('color', function (d) { return d; })
-         .style('background', 'white');
-       d3.select(this)
-         .append('div')
-         .html(function(d) { return data[d]; })
-         .style('color', function (d) { return data[d]; })
-         .style('background', 'white');
-       d3.select(this)
-         .append('div')
-         .html(function(d) { return d; })
-         .style('color', function (d) { return d; })
-         .style('background', 'black');
-       d3.select(this)
-         .append('div')
-         .html(function(d) { return data[d]; })
-         .style('color', function (d) { return data[d]; })
-         .style('background', 'black');
+       compare_color(d3.select(this), d);
+       compare_color(d3.select(this), data[d]);
     });
 }
 
