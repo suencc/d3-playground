b1dfbe15513708037d91f7cee3e38657d22969f0
Jialei666 <75222498@qq.com>
2016-02-20 11:08:18 +0800

    Added files via upload

---
+++ Jialei666/Mycode/HTMLElementpractice.js
@@ -0,0 +1,4 @@
+d3.select('body').append('a').attr('href','baidu.com').html('baidu').style('color','green').style('font-size','x-large');
+d3.select('body').append('a').attr('href','baidu.com').html('baidu').style('color','blue').style('font-size','x-large');
+d3.select('body').append('a').attr('href','baidu.com').html('baidu').style('color','red').style('font-size','x-large');
+d3.select('body').append('a').attr('href','baidu.com').html('baidu').style('color','yellow').style('font-size','x-large');
