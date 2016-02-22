bad1dfb2be12a8f3f12a7a587ecb620c081f9271
Jialei666 <75222498@qq.com>
2016-02-22 14:59:06 +0800

    Added files via upload

---
+++ Jialei666/Mycode/array.js
@@ -0,0 +1,13 @@
+﻿
+var temp;
+function print_arr(array){
+  var ul = d3.select('body').append('ul');
+  array.forEach(function(item,index,array) {
+    ul.append('li').html(item).attr('class','item-'+index);
+    console.log(array);
+  });
+}
+
+
+数组 = [1,2,3];
+print_arr(数组);
