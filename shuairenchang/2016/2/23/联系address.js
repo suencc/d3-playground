2f205985112e82f303319faae64ca54db88aa72f
shuairenchang <1164151045@qq.com>
2016-02-23 19:38:37 +0800

    Create 联系address.js

---
+++ shuairenchang/2016/2/23/联系address.js
@@ -0,0 +1,2 @@
+d3.select('body').append('address').html('如果你有问题与作者联系，请点击').append('a').html('作者网页').attr('href','http://www.baidu.com');
+d3.select('body').append('address').html('可以拨打联系').append('a').html('电话').attr('href','http://www.baidu.com')
