b159e97332da8102e07a7ad35a3f0e66b8218c37
Jialei666 <75222498@qq.com>
2016-02-29 13:51:55 +0800

    Added files via upload

---
+++ Jialei666/Mycode/Feb/29/test.js
@@ -0,0 +1,7 @@
+﻿/*data 和enter只是对数据进行操作并不输出*/
+var dataset = [ 10 , 20 , 30 , 40 , 50 ];  
+var p = d3.select('body').selectAll('p')
+          .data(dataset)
+          .enter()
+          .append('p')
+          .text(hello);
