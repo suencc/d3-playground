907152a3b13ab226c67c9dc97b27a0363c610af9
Sam Liu <ontouchstart@gmail.com>
2016-03-04 08:17:53 -0500

    Update css_link.js

--- ontouchstart/2016/03/04/css_link.js
+++ ontouchstart/2016/03/04/css_link.js
@@ -1,1 +1,1 @@
-d3.select('head').append('link').attr({rel: 'stylesheet', href: 'https://bigdata-mindstorms.github.io/d3-playground/ontouchstart/2016/03/04/background.css'});
+d3.select('head').append('link').attr({rel: 'stylesheet', href: '//bigdata-mindstorms.github.io/d3-playground/ontouchstart/2016/03/04/background.css'});
