84ca9c954f09988a8e16c37405e48969e3ba71a8
amarande <ash@roadsidepagoda.com>
2016-03-04 10:06:54 -0500

    Update css_link.js

--- amarande/2016/03/04/css_link.js
+++ amarande/2016/03/04/css_link.js
@@ -1,1 +1,7 @@
+---
+  attr:
+    rel: stylesheet
+    href: https://bigdata-mindstorms.github.io/d3-playground/amarande/2016/03/04/background.css
+---
 
+d3.select('head').append('link').attr({{page:attr | jsonify}});
