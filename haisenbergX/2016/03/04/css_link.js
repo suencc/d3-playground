2819562575636cc403066437ec430f27cc3a2b00
haisenbergX <671191287@qq.com>
2016-03-05 13:48:49 +0800

    Update css_link.js

--- haisenbergX/2016/03/04/css_link.js
+++ haisenbergX/2016/03/04/css_link.js
@@ -2,7 +2,7 @@
 ---
   attr:
     rel: stylesheet
-    href: https://bigdata-mindstorms.github.io/d3-playground/amarande/2016/03/04/background.css
+    href: https://bigdata-mindstorms.github.io/d3-playground/haisenbergX/2016/03/04/background.css
 ---
 
 d3.select('head').append('link').attr({{page:attr | jsonify}});
