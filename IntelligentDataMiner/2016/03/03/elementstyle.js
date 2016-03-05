589583d423ad07bf4584ee4c7b92e34ff544c3d0
IntelligentDataMiner <ethan.c.kwok@gmail.com>
2016-03-05 15:34:13 -0500

    Create elementstyle.js

---
+++ IntelligentDataMiner/2016/03/03/elementstyle.js
@@ -0,0 +1,7 @@
+d3.select('body')
+  .append('a')
+  .attr('href', 'https://bigdata-mindstorms.github.io/d3-playground/')
+  .html('a;lskfja;lksjf;laksjf;lksdjf')
+ .style('font-size', 'small')
+ .style('color', 'red')
+ 
