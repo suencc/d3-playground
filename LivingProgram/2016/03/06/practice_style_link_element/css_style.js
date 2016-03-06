111621e144899f53c31b5640ccc72e43a93e02f3
Kyle Han <kyhan316@gmail.com>
2016-03-06 15:44:03 -0500

    Update css_style.js

--- LivingProgram/2016/03/06/practice_style_link_element/css_style.js
+++ LivingProgram/2016/03/06/practice_style_link_element/css_style.js
@@ -1,1 +1,1 @@
-d3.text('//bigdata-mindstorms.github.io/d3-playground/LivingProgram/2016/03/06/background.css', function (e, d) {d3.select('head').append('style').html(d);})
+d3.text('//bigdata-mindstorms.github.io/d3-playground/LivingProgram/2016/03/06/practice_style_link_element/background.css', function (e, d) {d3.select('head').append('style').html(d);})
