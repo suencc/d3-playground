5d0cb52f166618b8357fb94df34495882fed948e
Kyle Han <kyhan316@gmail.com>
2016-03-06 15:43:49 -0500

    Update css_link.js

--- LivingProgram/2016/03/06/practice_style_link_element/css_link.js
+++ LivingProgram/2016/03/06/practice_style_link_element/css_link.js
@@ -1,6 +1,6 @@
 ---
 attr:
   rel: stylesheet
-  href: //bigdata-mindstorms.github.io/d3-playground/LivingProgram/2016/03/06/background.css
+  href: //bigdata-mindstorms.github.io/d3-playground/LivingProgram/2016/03/06/practice_style_link_element/background.css
 ---
 d3.select('head').append('link').attr({{ page.attr | jsonify}});
