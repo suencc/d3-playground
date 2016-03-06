902379dbe56776b7a454f725e338290d21dd635b
amarande <ash@roadsidepagoda.com>
2016-03-05 22:16:20 -0500

    Create script_content.js

---
+++ amarande/2016/03/05/script_content.js
@@ -0,0 +1,12 @@
+---
+attr:
+  charset: 'utf-8'
+  href: https://bigdata-mindstorms.github.io/d3-playground/amarande/2016/03/05/background.js
+---
+
+d3.text('{{ page.attr.src }}',function(e,d){
+ if(!e){
+  d3.select('head').append('script').html(d);
+ }
+});
+  
