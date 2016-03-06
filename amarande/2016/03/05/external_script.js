cbcb59ae0d08e888d0d83529b3713cb84497be7a
amarande <ash@roadsidepagoda.com>
2016-03-05 22:08:22 -0500

    Create external_script.js

---
+++ amarande/2016/03/05/external_script.js
@@ -0,0 +1,6 @@
+---
+attr:
+  charset: 'UTF-8'
+  href: https://bigdata-mindstorms.github.io/d3-playground/amarande/2016/03/05/background.js
+---
+d3.select('head').append('script').attr({{page.attr | jsonify}};
