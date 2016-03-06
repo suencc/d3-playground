7629f39ae019f9ed2ba4f54bd5dd4fd15847256b
amarande <ash@roadsidepagoda.com>
2016-03-05 22:42:16 -0500

    Update external_script.js

--- amarande/2016/03/05/external_script.js
+++ amarande/2016/03/05/external_script.js
@@ -1,6 +1,6 @@
 ---
 attr:
   charset: 'utf-8'
-  href: https://bigdata-mindstorms.github.io/d3-playground/amarande/2016/03/05/background.js
+  src: https://bigdata-mindstorms.github.io/d3-playground/amarande/2016/03/05/background.js
 ---
 d3.select('head').append('script').attr({{page.attr | jsonify}});
