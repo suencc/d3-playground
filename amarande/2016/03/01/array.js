1525514b531e2bad9d0631233a4e9f6e73fdfd24
amarande <ash@roadsidepagoda.com>
2016-03-06 16:59:38 -0500

    Create array.js

---
+++ amarande/2016/03/01/array.js
@@ -0,0 +1,14 @@
+title = '数组';
+d3.select('title').html(title);
+
+d3.select('body').append('h1').html('Main cities in Canada');
+var mainCities_Canada = ['Ottawa','Toronto','Vancouver','Edmonton','Montreal'];
+print_array(mainCities_Canada);
+
+function print_array(array){
+ var ul=d3.select('body').append('ul');
+ array.forEach(function(element,index,array){
+   ul.append('li').html(element).attr('class','item-'+index);
+   console.log(array);
+ });
+}
