44866ec31eea8791512402009887a4f198fa27bc
Kyle Han <kyhan316@gmail.com>
2016-03-06 11:00:05 -0500

    Update css_link.js

--- LivingProgram/2016/03/06/css_link.js
+++ LivingProgram/2016/03/06/css_link.js
@@ -1,1 +1,6 @@
-
+---
+attr:
+  rel: stylesheet
+  href: //bigdata-mindstorms.github.io/d3-playground/LivingProgram/2016/03/06/background.css
+---
+d3.select('head').append('link').attr({{ page.attr | jsonify}});
