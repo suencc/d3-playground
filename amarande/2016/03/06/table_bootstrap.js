4efc21f28728a2731a18129a243f78dd30c63971
amarande <ash@roadsidepagoda.com>
2016-03-07 11:06:26 -0500

    Create table_bootstrap.js

---
+++ amarande/2016/03/06/table_bootstrap.js
@@ -0,0 +1,10 @@
+d3.select('head').append('script').attr('src','//bigdata-mindstorms.github.io/d3-playground/amarande/2016/03/06/load_bootstrap.js');
+
+container = d3.select('body').append('div').attr('class','container');
+table = container.append('table').attr('class','table production');
+data = [[1,2,3],[4,5,6],[7,8,9]];
+table.append('tbody').selectAll('tr').data(data).enter().append('tr').each(function(d){
+  d3.select(this).selectAll('td').data(d).enter().append('td').html(function(d){return d;})});
+  
+  
+  
