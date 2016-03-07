bd15b649e7f4a0f63c621d7e8cb2350c65611cdb
VeroniqueSoleil <VeroniqueSoleil@users.noreply.github.com>
2016-03-07 16:46:06 +0800

    Update css_link.js

--- VeroniqueSoleil/2016/03/07/css_link.js
+++ VeroniqueSoleil/2016/03/07/css_link.js
@@ -1,6 +1,6 @@
 ---
 attr: 
   rel: stylesheet
-  href: //bigdata-mindstorms.github.io/d3-playground/VeroniqueSoleil/2016/03/07/background.css
+  href: //bigdata-mindstorms.github.io/d3-playground/VeroniqueSoleil/2016/03/07/Background.css
 ---
 d3.select('head').append('link').attr({{ page.attr | jsonify}} );
