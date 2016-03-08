c034838ba7c5d89d15a1973dcecad5050cd26be1
IntelligentDataMiner <ethan.c.kwok@gmail.com>
2016-03-07 21:45:41 -0500

    Update functions.js

--- IntelligentDataMiner/2016/03/03/functions.js
+++ IntelligentDataMiner/2016/03/03/functions.js
@@ -1,9 +1,8 @@
-
-d3.select('body').append('blockquote').html('Functions')
+d3.select('body').append('blockquote').html('Functions');
 
 
 function add_link(selection, href, html){
-  selection.append('a').attr('href', href).html(html)
+  selection.append('a').attr('href', href).html(html);
 }
 function print(selection, object){
   selection.append('pre').html(object.toString());
@@ -11,15 +10,13 @@ function print(selection, object){
 function alertf(html){
   alert(html);
 }
-
-function add(a,b){
-  a+b=c
-}
-
+function color(selection, htmls, colors){
+  selection.append('a').html(htmls).style('color', colors);
+} 
+color(d3.select('body'), 'ASDF', 'red');
+print(d3.select('body'), 'ajlfkajs;kldfja;klsdf');
 print(d3.select('body'), print);
 
-add_link(d3.select('body').append('p').html('This is the Link') ,'https://www.youtube.com/watch?v=dQw4w9WgXcQ', 'MDN Functions');
+add_link(d3.select('body').append('p').html('This is the Link') ,'https://www.youtube.com/watch?v=dQw4w9WgXcQ', ' LOL LCS VOTING');
 
-alertf(This is an Alert!);
-
-add(3,5)
+alertf('This is an Alert!')
