287d0800688b259bdc1bf2e4d9b60fe8da02013e
IntelligentDataMiner <ethan.c.kwok@gmail.com>
2016-03-07 20:38:15 -0500

    Update functions.js

--- IntelligentDataMiner/2016/03/03/functions.js
+++ IntelligentDataMiner/2016/03/03/functions.js
@@ -1,10 +1,25 @@
 
 d3.select('body').append('blockquote').html('Functions')
 
-function add_link(selection, href, html)
 
-function print
+function add_link(selection, href, html){
+  selection.append('a').attr('href', href).html(html)
+}
+function print(selection, object){
+  selection.append('pre').html(object.toString());
+}
+function alertf(html){
+  alert(html);
+}
 
-function alert
+function add(a,b){
+  a+b=c
+}
 
-function add
+print(d3.select('body'), print);
+
+add_link(d3.select('body').append('p').html('This is the Link') ,'https://www.youtube.com/watch?v=dQw4w9WgXcQ', 'MDN Functions');
+
+alertf(This is an Alert!);
+
+add(3,5)
