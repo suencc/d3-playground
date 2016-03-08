5fce15c5c7ba2ff00b2a15f226e06df26fbcf21e
Kyle Han <kyhan316@gmail.com>
2016-03-07 20:43:12 -0500

    Create load_bootstrap.js

---
+++ LivingProgram/2016/03/07/load_bootstrap.js
@@ -0,0 +1,16 @@
+---
+css:
+  rel: stylesheet
+  href: //cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap.min.css
+theme:
+  rel: stylesheet
+  href: //cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap-theme.min.css
+jquery:
+  src: //cdn.bootcss.com/jquery/1.11.3/jquery.min.js
+js:
+  src: //cdn.bootcss.com/bootstrap/3.3.5/js/bootstrap.min.js
+---
+d3.select('head').append('link').attr({{ page.css | jsonify }});
+d3.select('head').append('link').attr({{ page.theme | jsonify }});
+d3.select('head').append('script').attr({{ page.jquery | jsonify }});
+d3.select('head').append('script').attr({{ page.js | jsonify }});
