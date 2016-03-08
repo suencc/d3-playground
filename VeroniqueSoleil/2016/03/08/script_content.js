0916a42a8db57414ab6849c4f4454f42f12922ec
VeroniqueSoleil <VeroniqueSoleil@users.noreply.github.com>
2016-03-08 16:06:42 +0800

    Update script_content.js

--- VeroniqueSoleil/2016/03/08/script_content.js
+++ VeroniqueSoleil/2016/03/08/script_content.js
@@ -1,7 +1,7 @@
 ---
 attr:
   charset: 'utf-8'
-  scr: //bigdata-mindstorms.github.io/d3-playground/VeroniqueSoleil/2016/03/08/background.js
+  src: //bigdata-mindstorms.github.io/d3-playground/VeroniqueSoleil/2016/03/08/background.js
 ---
 d3.text('{{ page.attr.src }}', function (e, d) {
   if(!e){
