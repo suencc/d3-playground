1e61470d6dfcd3c5b9e53aa84015d3c7d59f5b12
Kyle Han <kyhan316@gmail.com>
2016-03-08 20:57:27 -0500

    Update bootstrap_table.js

--- LivingProgram/2016/03/08/bootstrap_table.js
+++ LivingProgram/2016/03/08/bootstrap_table.js
@@ -1,1 +1,8 @@
+d3.select('head').append('script').attr('src', '//bigdata-mindstorms.github.io/d3-playground/LivingProgram/2016/03/08/load_bootstrap.js');
 
+container = d3.select('body').append('div').attr('class', 'container');
+table = container.append('table').attr('class', 'table table-hover');
+data = [[9, 8, 7], [6, 5, 4], [3, 2, 1]];
+table.append('tbody').selectAll('tr').data(data).enter().append('tr').each(function (d){
+  d3.select(this).selectAll('td').data(d).enter().append('td').html(function (d) { return d; })
+});
