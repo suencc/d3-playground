3d9219c2b41a5d42a4c29d4fc78f86770521a085
Kyle Han <kyhan316@gmail.com>
2016-03-08 21:04:06 -0500

    Update bootstrap_table.js

--- LivingProgram/2016/03/08/bootstrap_table.js
+++ LivingProgram/2016/03/08/bootstrap_table.js
@@ -2,7 +2,11 @@ d3.select('head').append('script').attr('src', '//bigdata-mindstorms.github.io/d
 
 container = d3.select('body').append('div').attr('class', 'container');
 table = container.append('table').attr('class', 'table table-hover');
-data = [[9, 8, 7], [6, 5, 4], [3, 2, 1]];
-table.append('tbody').selectAll('tr').data(data).enter().append('tr').each(function (d){
+body_data = [[9, 8, 7], [6, 5, 4], [3, 2, 1]];
+table.append('tbody').selectAll('tr').data(body_data).enter().append('tr').each(function (d){
   d3.select(this).selectAll('td').data(d).enter().append('td').html(function (d) { return d; })
+});
+head_data = [Numbers 9-7, Numbers 6-4, Numbers 3-1];
+table.append('thead').selectAll('tr').data(head_data).enter().append('tr').each(function (d){
+  d3.select(this).selectAll('td').data(d).enter().append('td').html(function (d) { return d;})
 });
