50d1d24b078b1bbe8e4ce6fb29d998ec30c0daca
ontouchstart <ontouchstart@gmail.com>
2016-03-11 14:41:25 -0500

    d3-4.0.0-alpha.28

---
+++ ontouchstart/2016/03/12/4.0.0-alpha.28/main.js
@@ -0,0 +1,3 @@
+window.onload = function () {
+  d3.select('head').append('title').html(`d3-${d3.version}`);
+};
---
+++ ontouchstart/2016/03/12/README.md
@@ -0,0 +1,2 @@
+
+http://bigdata-mindstorms.github.io/d3-playground/ontouchstart/2016/03/12/4.0.0-alpha.28/
