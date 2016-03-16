e47a0cf7bf2d038aa58b2ee09ee350ab573596a5
ontouchstart <ontouchstart@gmail.com>
2016-03-16 12:18:20 -0400

    load_bootsrap.js : load js libraries

--- ontouchstart/2016/03/16/load_bootstrap.js
+++ ontouchstart/2016/03/16/load_bootstrap.js
@@ -9,6 +9,12 @@ function load_bootstrap() {
       .attr('rel', 'stylesheet')
       .attr('href', '/d3-playground/bootstrap/3.3.6/css/bootstrap-theme.min.css');
     this.bootstrap_version = '3.3.6';
+    d3.select('head')
+      .append('script')
+      .attr('src', '/d3-playground/jquery/2.2.1/jquery.min.js');
+    d3.select('head')
+      .append('script')
+      .attr('src', '/d3-playground/bootstrap/3.3.6/js/bootstrap.min.js');
     console.log(this);
   }
 }
