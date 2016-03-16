10d85696dfd89fccac48ceb9abf45ee11ebcbf47
VeroniqueSoleil <VeroniqueSoleil@users.noreply.github.com>
2016-03-16 13:45:16 +0800

    Create function_test.js

---
+++ VeroniqueSoleil/2016/03/16/function_test.js
@@ -0,0 +1,6 @@
+function f(c,d) {
+  c.html(d);
+}
+c = d3.select('body').append('button').append('a').attr('href','http://www.goodreads.com/book/show/22948457-digital-humanitarians?from_new_nav=true&ac=1&from_search=true');
+d = 'Digital Humanitarians';
+f (c,d);
